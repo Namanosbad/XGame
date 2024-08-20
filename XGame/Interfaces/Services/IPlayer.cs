00ad2642b0b108387dd69c95c01df374b534a98b
Matheus <matheus.limamst@gmail.com>
2024-08-20 16:35:04 -0300

    Create Interfaces

---
+++ XGame/Interfaces/Services/IPlayer.cs
@@ -0,0 +1,11 @@
+﻿using XGame.Domain.Arguments.Player;
+
+namespace XGame.Domain.Interfaces.Services
+{
+    public interface IPlayer
+    {
+        AuthenticatePlayerResponse AuthenticatePlayer (string Email, string Password);
+
+        AddPlayerResponse AddPlayer (AddPlayerRequest request)
+    }
+}
